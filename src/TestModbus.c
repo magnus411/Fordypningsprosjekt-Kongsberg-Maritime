f6fa7e8e49f80f122e8d40b760de85565ec77f7c
magnus411 <magnusgjerstad00@gmail.com>
2024-09-23 15:47:00 +0200

    Added two threads for two simultaneous connections writing to same buffer

--- src/TestModbus.c
+++ src/TestModbus.c
@@ -3,16 +3,35 @@
 #include <pthread.h>
 #include "sdb.h"
 
+static CircularBuffer Cb = { 0 };
+
 int
 TestModbus(void)
 {
-    CircularBuffer Cb;
     InitCircularBuffer(&Cb, SdbMebiByte(16));
 
+    // First Modbus Server
+    ModbusArgs MbArgs;
+    MbArgs.PORT = 3490;
+    MbArgs.Cb   = &Cb;
+    strncpy(MbArgs.Ip, "127.0.0.1", 10);
+
+    // Second Modbus Server
+    ModbusArgs MbArgs2;
+    MbArgs2.PORT = 3491;
+    MbArgs2.Cb   = &Cb;
+    strncpy(MbArgs2.Ip, "127.0.0.1", 10);
+
+    // Connecting to first Modbus server
     pthread_t modbus_tid;
-    pthread_create(&modbus_tid, NULL, ModbusThread, &Cb);
+    pthread_create(&modbus_tid, NULL, ModbusThread, &MbArgs);
+
+    // Connecting to second Modbus server
+    pthread_t modbus_tid2;
+    pthread_create(&modbus_tid2, NULL, ModbusThread, &MbArgs2);
 
     pthread_join(modbus_tid, NULL);
+    pthread_join(modbus_tid2, NULL);
 
     FreeCircularBuffer(&Cb);
 
