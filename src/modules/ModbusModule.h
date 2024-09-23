0e4d535e279cb351e6aed09e3cfd09835ee5c565
magnus411 <magnusgjerstad00@gmail.com>
2024-09-23 15:44:47 +0200

    Made struct for passing args to thread

--- src/modules/ModbusModule.h
+++ src/modules/ModbusModule.h
@@ -2,6 +2,23 @@
 #define MODBUSMODULE_H
 
 #include "../CircularBuffer.h"
+#include <stdint.h>
+
+typedef struct
+{
+    int     Protocol;
+    int     UnitId;
+    uint8_t Data[MAX_DATA_LENGTH];
+    size_t  DataLength;
+} QueueItem;
+
+typedef struct
+{
+    int             PORT;
+    char            Ip[10];
+    CircularBuffer *Cb;
+
+} ModbusArgs;
 
 void *ModbusThread(void *arg);
 
