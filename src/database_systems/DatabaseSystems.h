dcbef89a798eef55bf6620c5102800af90790fe1
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-10-11 16:55:27 +0200

    Conditional compilation of used database API implementation

--- src/database_systems/DatabaseSystems.h
+++ src/database_systems/DatabaseSystems.h
@@ -3,9 +3,11 @@
 
 #define DATABASE_SYSTEM_POSTGRES 1
 
-#define USED_DATABASE_SYSTEM 1
+#ifndef DATABASE_SYSTEM
+#define DATABASE_SYSTEM 1
+#endif
 
-#if USED_DATABASE_SYSTEM == DATABASE_SYSTEM_POSTGRES
+#if DATABASE_SYSTEM == DATABASE_SYSTEM_POSTGRES
 #include <database_systems/Postgres.h>
 #else
 #error "Database system not supported"
