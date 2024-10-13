b5d2c7acc22bb616d71ef241fcd31195804d8bde
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-10-13 23:16:09 +0200

    Updated postgres api

--- src/DatabaseSystems/DatabaseSystems.c
+++ src/DatabaseSystems/DatabaseSystems.c
@@ -1,11 +1,11 @@
-#include <DatabaseSystems/DatabaseSystems.h>
-#include <Modules/DatabaseModule.h>
+#include <src/DatabaseSystems/DatabaseSystems.h>
+#include <src/Modules/DatabaseModule.h>
 
 // This is used in DatabaseModule.c by referencing the database systems using extern
 
 #ifdef DATABASE_SYSTEM_POSTGRES
 static database_api PgApi__
-    = { .Init = PgInit, .Run = PgRun, .Context = NULL, .IsInitialized = false };
+    = { .Init = PgInit, .Run = PgRun, .Finalize = PgFinalize, .Ctx = NULL, .IsInitialized = false };
 database_api *DbSystemPostgres__ = &PgApi__;
 #else
 database_api *DbPostgres__ = NULL;
