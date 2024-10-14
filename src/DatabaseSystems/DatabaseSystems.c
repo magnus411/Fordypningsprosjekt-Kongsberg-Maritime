72f0af28087a8fd1c74cc6745ad9c5fe631b474d
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-10-14 15:02:50 +0200

    Moved database_api to DatabaseSystems.h and made API init function

--- src/DatabaseSystems/DatabaseSystems.c
+++ src/DatabaseSystems/DatabaseSystems.c
@@ -1,13 +1,55 @@
+#include <src/Sdb.h>
+SDB_LOG_REGISTER(DbSystems);
+
+#include <src/Common/SdbErrno.h>
+#include <src/Common/SensorDataPipe.h>
 #include <src/DatabaseSystems/DatabaseSystems.h>
 #include <src/DatabaseSystems/Postgres.h>
 #include <src/Modules/DatabaseModule.h>
 
-// This is used in DatabaseModule.c by referencing the database systems using extern
-
 #if DATABASE_SYSTEM_POSTGRES == 1
-static database_api PgApi__
-    = { .Init = PgInit, .Run = PgRun, .Finalize = PgFinalize, .Ctx = NULL, .IsInitialized = false };
-database_api *DbSystemPostgres__ = &PgApi__;
+static bool PgIsAvailable_ = true;
 #else
-database_api *DbPostgres__ = NULL;
+static bool PgApiIsAvailable_ = false;
 #endif // DATABASE_SYSTEM_POSTGRES
+
+bool
+DbsDatabaseIsAvailable(Db_System_Id DbsId)
+{
+    switch(DbsId) {
+        case Dbs_Postgres:
+            return PgIsAvailable_;
+        default:
+            return false;
+    }
+}
+
+sdb_errno
+DbsInitApi(Db_System_Id DbsId, sensor_data_pipe *SdPipe, sdb_arena *Arena, u64 ArenaSize,
+           database_api *Dbs)
+{
+    switch(DbsId) {
+        case Dbs_Postgres:
+            {
+                if(!DbsDatabaseIsAvailable(DbsId)) {
+                    SdbLogWarning("PostgreSQL is unavailable");
+                    return -SDBE_DBS_UNAVAIL;
+                }
+                Dbs->Init     = PgInit;
+                Dbs->Run      = PgRun;
+                Dbs->Finalize = PgFinalize;
+            }
+            break;
+        default:
+            {
+                SdbLogError("Database id doesn't exist");
+                return -EINVAL;
+            }
+            break;
+    }
+
+    SdbMemcpy(&Dbs->SdPipe, SdPipe, sizeof(sensor_data_pipe));
+    SdbArenaBootstrap(&Dbs->Arena, Arena, ArenaSize);
+
+    return 0;
+}
