ab71b903e42c8455469f5a7e79e397e23c5f5760
Magnus <magnusgjerstad00@gmail.com>
2024-10-14 05:54:14 -0700

    removed testing main

--- src/Main.c
+++ src/Main.c
@@ -1,44 +1,13 @@
-#include <stdlib.h>
-#include <libpq-fe.h>
+
 
 #define SDB_LOG_LEVEL 4
 #include <Sdb.h>
 
-SDB_LOG_REGISTER(Main);
-
-#include <common/CircularBuffer.h>
-#include <comm_protocols/CommProtocols.h>
+SDB_LOG_REGISTER(mainasd);
 
 int
-main(int ArgCount, char **ArgV)
+main()
 {
-
-    circular_buffer Cb;
-    CbInit(&Cb, 16000);
-
-    comm_protocol_api ProtocolModbus;
-    protocol_args     Args
-        = { .Type = PROTOCOL_MODBUS_TCP, .Address = "127.0.0.1", .Port = 3490, .Cb = &Cb };
-
-    PickProtocol(PROTOCOL_MODBUS_TCP, &ProtocolModbus);
-    ProtocolModbus.Initialize(&ProtocolModbus, &Args);
-    ProtocolModbus.StartComm(&ProtocolModbus);
-
-    comm_protocol_api ProtocolMQTT;
-    protocol_args     ArgsMQTT = { .Type    = PROTOCOL_MQTT,
-                                   .Address = "tcp://127.0.0.1:1883",
-                                   .Topic   = "MODBUS",
-                                   .Qos     = 0,
-                                   .Cb      = &Cb };
-
-    PickProtocol(PROTOCOL_MQTT, &ProtocolMQTT);
-    ProtocolMQTT.Initialize(&ProtocolMQTT, &ArgsMQTT);
-
-    ProtocolMQTT.StartComm(&ProtocolMQTT);
-
-    while(1) {
-        sleep(1);
-    }
-
+    printf("hello");
     return 0;
 }
