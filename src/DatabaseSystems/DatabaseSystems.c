d0051a63d753351ea4cf27b741b6086fca48f61b
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-10-14 11:17:15 +0200

    Corrected #if check

--- src/DatabaseSystems/DatabaseSystems.c
+++ src/DatabaseSystems/DatabaseSystems.c
@@ -1,12 +1,13 @@
 #include <src/DatabaseSystems/DatabaseSystems.h>
+#include <src/DatabaseSystems/Postgres.h>
 #include <src/Modules/DatabaseModule.h>
 
 // This is used in DatabaseModule.c by referencing the database systems using extern
 
-#ifdef DATABASE_SYSTEM_POSTGRES
+#if DATABASE_SYSTEM_POSTGRES == 1
 static database_api PgApi__
     = { .Init = PgInit, .Run = PgRun, .Finalize = PgFinalize, .Ctx = NULL, .IsInitialized = false };
 database_api *DbSystemPostgres__ = &PgApi__;
 #else
 database_api *DbPostgres__ = NULL;
-#endif
+#endif // DATABASE_SYSTEM_POSTGRES
