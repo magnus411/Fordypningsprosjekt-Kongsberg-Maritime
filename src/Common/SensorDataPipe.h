13384e40250958d8c23a868aa0171585fb58a9c3
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-10-19 21:02:57 +0200

    Added insert function for sensor data pipe

--- src/Common/SensorDataPipe.h
+++ src/Common/SensorDataPipe.h
@@ -1,6 +1,8 @@
 #ifndef SENSOR_DATA_PIPE_H
 #define SENSOR_DATA_PIPE_H
 
+#include <stdio.h>
+
 #include <src/Common/CircularBuffer.h>
 
 // TODO(ingar): Some way of tagging buffers for priorities or similar
@@ -13,5 +15,6 @@ typedef struct
 } sensor_data_pipe;
 
 sdb_errno SdPipeInit(sensor_data_pipe *SdPipe, u64 BufCount, size_t BufSizes[], sdb_arena *Arena);
+ssize_t   SdPipeInsert(sensor_data_pipe *SdPipe, u64 Buf, void *Data, size_t Size);
 
 #endif
