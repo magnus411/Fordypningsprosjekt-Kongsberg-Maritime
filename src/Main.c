6043b854e4a39ccf5f9c9d1ede70ae2338de0c68
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-10-19 21:04:06 +0200

    Added comm module thread creation to main

--- src/Main.c
+++ src/Main.c
@@ -10,13 +10,16 @@
 SDB_LOG_REGISTER(Main);
 
 #include <src/Common/SensorDataPipe.h>
+#include <src/DatabaseSystems/DatabaseInitializer.h>
 #include <src/DatabaseSystems/DatabaseSystems.h>
+#include <src/Modules/CommModule.h>
 #include <src/Modules/DatabaseModule.h>
 
 #define SD_PIPE_BUF_COUNT 4
 
 // TODO(ingar): Is it fine to keep this globally?
-static i64 NextDbmTId_ = 1;
+static i64 NextDbmTId_  = 1;
+static i64 NextCommTId_ = 1;
 
 int
 main(int ArgCount, char **ArgV)
@@ -24,7 +27,6 @@ main(int ArgCount, char **ArgV)
     sdb_arena SdbArena;
     u64       SdbArenaSize = SdbMebiByte(32);
     u8       *SdbArenaMem  = malloc(SdbArenaSize);
-
     if(NULL == SdbArenaMem) {
         SdbLogError("Failed to allocate memory for arena");
         exit(EXIT_FAILURE);
@@ -34,34 +36,39 @@ main(int ArgCount, char **ArgV)
 
     size_t BufSizes[SD_PIPE_BUF_COUNT]
         = { SdbKibiByte(32), SdbKibiByte(32), SdbKibiByte(32), SdbKibiByte(32) };
-
     sensor_data_pipe *SdPipe      = SdbPushStruct(&SdbArena, sensor_data_pipe);
     sdb_errno         PipeInitRet = SdPipeInit(SdPipe, SD_PIPE_BUF_COUNT, BufSizes, &SdbArena);
     if(PipeInitRet != 0) {
         SdbLogError("Failed to init sensor data pipe");
         exit(EXIT_FAILURE);
     }
-    SdbArenaInit(&MainArena, ArenaMemory, ArenaMemorySize);
-
 
     db_module_ctx *DbModuleCtx = SdbPushStruct(&SdbArena, db_module_ctx);
-    DbModuleCtx->DbsToRun      = Dbs_Postgres;
     DbModuleCtx->ThreadId      = NextDbmTId_++;
+    DbModuleCtx->DbsType       = Dbs_Postgres;
+    DbModuleCtx->ArenaSize     = SdbMebiByte(9);
+    DbModuleCtx->DbsArenaSize  = SdbMebiByte(8);
+    SdbArenaBootstrap(&SdbArena, &DbModuleCtx->Arena, DbModuleCtx->ArenaSize);
+    SdbMemcpy(&DbModuleCtx->SdPipe, SdPipe, sizeof(*SdPipe));
 
-    SdbArenaBootstrap(&SdbArena, &DbModuleCtx->Arena, SdbMebiByte(9));
-    SdbMemcpy(&DbModuleCtx->SdPipe, SdPipe, sizeof(sensor_data_pipe));
+    comm_module_ctx *CommModuleCtx = SdbPushStruct(&SdbArena, comm_module_ctx);
+    CommModuleCtx->ThreadId        = NextCommTId_++;
+    CommModuleCtx->CpType          = Comm_Protocol_Modbus_TCP;
+    CommModuleCtx->ArenaSize       = SdbMebiByte(9);
+    CommModuleCtx->CpArenaSize     = SdbMebiByte(8);
+    SdbArenaBootstrap(&SdbArena, &CommModuleCtx->Arena, CommModuleCtx->ArenaSize);
+    SdbMemcpy(&CommModuleCtx->SdPipe, SdPipe, sizeof(*SdPipe));
 
-    pthread_t DbThread;
-    pthread_create(&DbThread, NULL, DbModuleRun, DbModuleCtx);
-    pthread_join(DbThread, NULL);
+    pthread_t DbmThread;
+    pthread_t CommThread;
+    pthread_create(&DbmThread, NULL, DbModuleRun, DbModuleCtx);
+    pthread_create(&CommThread, NULL, CommModuleRun, CommModuleCtx);
+    pthread_join(CommThread, NULL);
+    pthread_join(DbmThread, NULL);
 
-    if(DbModuleCtx->Errno != 0) {
-        SdbLogError("Database module thread %ld, running database %s, finished with error: %d",
-                    DbModuleCtx->ThreadId, DbsIdToName(DbModuleCtx->DbsToRun), DbModuleCtx->Errno);
-        exit(EXIT_FAILURE);
-    } else {
-        SdbLogInfo("Database module thread %ld, running database %s, finished successfully",
-                   DbModuleCtx->ThreadId, DbsIdToName(DbModuleCtx->DbsToRun));
+    if(DbModuleCtx->Errno == 0 && CommModuleCtx->Errno == 0) {
         exit(EXIT_SUCCESS);
+    } else {
+        exit(EXIT_FAILURE);
     }
 }
