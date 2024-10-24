07b92c4910f7037a4c93b5532bf6e902663c5942
Magnus <magnusgjerstad00@gmail.com>
2024-10-24 13:12:03 -0700

    Updated temp main to test metric lib

--- src/Main.c
+++ src/Main.c
@@ -2,6 +2,7 @@
 #include <libpq-fe.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define SDB_H_IMPLEMENTATION
 #include <src/Sdb.h>
@@ -12,10 +13,9 @@ SDB_LOG_REGISTER(Main);
 #include <src/Common/SensorDataPipe.h>
 #include <src/DatabaseSystems/DatabaseSystems.h>
 #include <src/Modules/DatabaseModule.h>
+
+
 #include <src/Metrics.h>
-
-
-#include <unistd.h>
 
 #define SD_PIPE_BUF_COUNT 4
 
@@ -25,58 +25,75 @@ static i64 NextDbmTId_ = 1;
 circular_buffer CB;
 
 
+void *
+tempInsert(void *)
+{
+    size_t data_size = 256;
+    char  *data      = malloc(data_size);
+    memset(data, 0, data_size);
 
-
-void *tempInsert(void*){
- while(1) {
-        int data = 22;
-        CbInsert(&CB, &data, sizeof(int));
-        
-        usleep(100000);
-
-
-        
+    while(1) {
+        CbInsert(&CB, data, data_size);
     }
-}
 
-void *tempPop(void*){
- while(1) {
-        int data;
-        CbRead(&CB, &data, sizeof(int));
-        
-        usleep(100000);
-
-
-        
-    }
+    free(data);
 }
 
 
-int main(int argc, char const *argv[]) {
-    
+void *
+tempPop(void *)
+{
+    size_t data_size = 256;
+    char  *data      = malloc(data_size);
+
+    while(1) {
+        CbRead(&CB, data, data_size);
+    }
+
+    free(data);
+}
+
+
+int
+main(int argc, char const *argv[])
+{
+
+
+    InitMetric(&OccupancyMetric, METRIC_OCCUPANCY, "occupancy_metrics.sdb");
+
+
+    InitMetric(&InputThroughput, METRIC_INPUT_THROUGHPUT, "input_throughput_metrics.sdb");
+    InitMetric(&BufferWriteThroughput, METRIC_BUFFER_WRITE_THROUGHPUT,
+               "buffer_write_throughput_metrics.sdb");
+    InitMetric(&BufferReadThroughput, METRIC_BUFFER_READ_THROUGHPUT,
+               "buffer_read_throughput_metrics.sdb");
+    InitMetric(&OutputThroughput, METRIC_OUTPUT_THROUGHPUT, "output_throughput_metrics.sdb");
+
 
     sdb_arena SdbArena;
     u64       SdbArenaSize = SdbMebiByte(32);
     u8       *SdbArenaMem  = malloc(SdbArenaSize);
 
     SdbArenaInit(&SdbArena, SdbArenaMem, SdbArenaSize);
-    CbInit(&CB, SdbKiloByte(0.5),&SdbArena);
-
-    metric_type type = METRIC_OCCUPANCY; 
-    InitMetricSystem(type); 
-
+    CbInit(&CB, SdbKiloByte(100), &SdbArena);
 
 
     pthread_t temp1;
     pthread_t temp2;
 
     pthread_create(&temp1, NULL, tempInsert, NULL);
-    pthread_create(&temp2, NULL, tempOcc, NULL);
+    pthread_create(&temp2, NULL, tempPop, NULL);
 
 
     pthread_join(temp1, NULL);
     pthread_join(temp2, NULL);
 
+
+    DestroyMetric(&OccupancyMetric);
+    DestroyMetric(&InputThroughput);
+    DestroyMetric(&BufferWriteThroughput);
+    DestroyMetric(&BufferReadThroughput);
+    DestroyMetric(&OutputThroughput);
 
 
     return 0;
