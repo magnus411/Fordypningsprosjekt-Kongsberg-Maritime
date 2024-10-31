b4c37dac855ee150d6564752fed8c0d2baca9591
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-10-31 17:04:16 +0100

    Moved ReceiveWithTimeout function to Socket

--- src/CommProtocols/Modbus.c
+++ src/CommProtocols/Modbus.c
@@ -32,54 +32,6 @@ MbThreadArenasInit(void)
     SdbThreadArenasInit(Modbus);
 }
 
-// Returns:
-//   >0: number of bytes received
-//    0: server closed connection
-//   -1: error occurred
-//   -2: timeout occurred
-int
-ReceiveWithTimeout(int SockFd, void *Buffer, size_t Length, int TimeoutSecs)
-{
-    fd_set         ReadFds;
-    struct timeval Timeout;
-
-    // Setup for select()
-    FD_ZERO(&ReadFds);
-    FD_SET(SockFd, &ReadFds);
-
-    // Set timeout
-    Timeout.tv_sec  = TimeoutSecs;
-    Timeout.tv_usec = 0;
-
-    // Wait for data or timeout
-    int SelectResult = select(SockFd + 1, &ReadFds, NULL, NULL, &Timeout);
-
-    if(SelectResult == -1) {
-        // Select error
-        SdbLogError("Select failed: %s", strerror(errno));
-        return -1;
-    } else if(SelectResult == 0) {
-        // Timeout
-        SdbLogError("Receive timeout after %d seconds", TimeoutSecs);
-        return -2;
-    }
-
-    // Data is available, perform the recv
-    int RecvResult = recv(SockFd, Buffer, Length, 0);
-
-    if(RecvResult == 0) {
-        // Server closed connection
-        SdbLogError("Server closed connection");
-        return 0;
-    } else if(RecvResult == -1) {
-        SdbLogError("Recv failed: %s", strerror(errno));
-        return -1;
-    }
-
-    return RecvResult;
-}
-
-
 ssize_t
 MbReceiveTcpFrame(int Sockfd, u8 *Frame, size_t BufferSize)
 {
--- src/Common/Socket.h
+++ src/Common/Socket.h
@@ -3,12 +3,15 @@
 
 #include <src/Sdb.h>
 
+#include <src/Common/Time.h>
+
 SDB_BEGIN_EXTERN_C
 
 #include <sys/socket.h>
 #include <sys/types.h>
 
-int CreateSocket(const char *IpAddress, int Port);
+int SocketCreate(const char *IpAddress, int Port);
+int SocketRecvWithTimeout(int SockFd, void *Buffer, size_t Length, sdb_timediff Timeout);
 
 SDB_END_EXTERN_C
 
